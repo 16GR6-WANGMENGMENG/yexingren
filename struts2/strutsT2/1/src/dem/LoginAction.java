d680af59969385a24f00fd425130e56b8f98c478
“wwl145869” <1272913141@qq.com>
2019-01-13 17:44:31 +0800

    开发五部 王万里
    “”

---
+++ struts2/strutsT2/1/src/dem/LoginAction.java
@@ -0,0 +1,30 @@
+package dem;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport {
+
+	private String name;
+	private String password;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String execute() throws Exception {
+		if("wwl".equals(name) && "123".equals(password)){
+			return  SUCCESS;
+		}else{
+			return ERROR;
+		}
+	}
+}
